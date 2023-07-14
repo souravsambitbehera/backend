a1af3b7d5761cac426bdc67da9ee120765338abf
sourav sambit behera <souravsambit12@gmail.com>
2023-07-14 10:26:52 +0530

    brach check

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const server = express()
 const productRouter = require('./routes/productRoutes')
 const userRouter = require('./routes/userRoutes')
 const port = 8080
-// bodyparser
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ecomerce');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 server.use(express.json());
 server.use(morgan('dev'))
